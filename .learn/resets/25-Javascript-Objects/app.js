45c5057724ddf7b0dd065cb5b163f0ae36e4e42a
Jesús Cano <jcanort@gmail.com>
2021-09-29 08:29:04 +0000

    Js Exercise

--- .learn/resets/25-Javascript-Objects/app.js
+++ .learn/resets/25-Javascript-Objects/app.js
@@ -16,48 +16,23 @@ var person2 = {
     significant_other: person
 };
 
-var person3 = {
-    name: "Jimmy",
-    lastname: "Doe",
-    age: 13,
-    gender: "male",
-    lucky_numbers: [ 1, 2, 3, 4],
-    significant_other: null
-};
-
-
 var family = {
     lastname: "Doe",
-    members: [person, person2, person3]   //Array of objects, don't forget to add Jimmy
+    members: [person, person2]   //Array of objects, don't forget to add Jimmy
 };
 
 
 function addAllFamilyLuckyNumbers(anArray){
-  
   var sumOfAllLuckyNumbers = 0; //sumOfAllLuckyNumbers is a number, the sum of all lucky numbers.
-
-  for (let i = 0; i < anArray.length; i++) { //Va de person a person 2 a person 3
-
-    //estoy dentro del objeto person
-
-    for (let j = 0; j < 4; j++) { //entro dentro de person.lucky_numbers
-      
-      //console.log(anArray[i].lucky_numbers[j]);
-      sumOfAllLuckyNumbers += anArray[i].lucky_numbers[j];
-
-    }
-    
-  }
 
   //To-Do: loop and add; consider nested loops
   //Hint: use the anArray variable to get all of the lucky numbers
-
   
   return sumOfAllLuckyNumbers;
 }
 
 //Enter all your code here:
-person.lucky_numbers[3] = 33; 
+
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
--- exercises/24-Bottles-of-milk/app.js
+++ exercises/24-Bottles-of-milk/app.js
@@ -1,13 +1,16 @@
 // Your code here:
-for (let i = 99; i >= 0; i--) {    
-    //More than 1 bottle
-    if(i > 0){
-        console.log(i + ' botellas');
+for (let i = 99; i >= 0; i--) {
+    
+    //0 bottle
+    if(i  == 0) {
+        console.log('No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.');
     }
     //One bottle
-
-
+    else if (i == 1){
+        console.log('1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.');
+    }
     //0 bottle 
-    
-    
+    else{
+        console.log(i + ' bottles of milk on the wall, ' + i + ' bottles of milk. Take one down and pass it around, ' + (i-1) + ' bottles of milk on the wall.');
+    }    
 }
